21f904a4818f2505cd81364a19e57697e52d4013
Zhumeng99 <1410157844@qq.com>
2023-06-01 22:46:13 +0800

    fix(other): 完成

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo bg-red-600" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
